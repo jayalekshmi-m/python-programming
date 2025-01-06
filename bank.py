3b97fd83a7df1656dd3192a80108712cdf68b12e
Jayalekshmi M <ammusz749@gmail.com>
2025-01-06 21:06:11 +0530

    Add files via upload

--- bank.py
+++ bank.py
@@ -1,21 +1,47 @@
 class Bank:
-    def __init__(self,accno,name,typpe,balance):
-        self.accno=accno
+    def __init__(self,acc_no,name,balance=0,acc_type="Savings"):
+        self.acc_no=acc_no
         self.name=name
-        self.type=typpe
+        self.acc_type=acc_type
         self.balance=balance
-    def deposit(self):
-        amount=int(input("Enter the amount to deposit :"))
-        self.balance+=amount
-        print("Deposit succesfull")
-        print("Your current status is :")
-        print("Account no : {self.accno}")
-        print("Account holder name : {self.name}")
-        print("Account type : {self.typpe}")
-        print("Current balane : Rs {self.balance}")
-    def withdraw(self):
-        amount1=int(input("Enter the amount to be withdrawn :"))
-        if amount>self.balance:
-            print("Not enough balance")
+    def checkbalance(self):
+        print(f"Your account balance is: {self.balance}")
+    def withdraw(self,withdraw_amount):
+        if withdraw_amount>self.balance:
+            print("Insufficient Balance")
         else:
-            self.balance-=amount1
+            print(f"{withdraw_amount} is debitted")
+            self.balance=self.balance-withdraw_amount
+            print(f"Your current balance is: {self.balance}")
+    def deposit(self,deposit_amount):
+        self.balance+=deposit_amount
+        print(f"Amount deposited:{deposit_amount}")
+        print(f"Your current balance is: {self.balance}")
+    def AccountStatus(self):
+        print(f"Name of account holder: {self.name}")
+        print(f"Account Number: {self.acc_no}")
+        print(f"Account Type: {self.acc_type}")
+
+obj=Bank(88480,"Ammu",50000)
+
+while(1):
+    choice=int(input("Enter a choice\n1.Check Balance\n2.Withdrawal\n3.Deposit Amount\n4.Account Status\n"))
+    if choice==1:
+        obj.checkbalance()
+    elif choice==2:
+        withdrawal_amount=int(input("Enter the amount you want to withdraw: "))
+        obj.withdraw(withdrawal_amount)
+       
+    elif choice==3:
+        deposit_amount=int(input("enter the amount you want to deposit: "))
+        obj.deposit(deposit_amount)
+        
+    elif choice==4:
+        obj.AccountStatus()
+
+        # oop
+        
+    
+        
+        
+    
