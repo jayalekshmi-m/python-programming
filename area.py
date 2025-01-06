27e906deb1e85e6b75886c079e631ce0427064e8
Jayalekshmi M <ammusz749@gmail.com>
2025-01-06 21:07:51 +0530

    Add files via upload

--- area.py
+++ area.py
@@ -1,6 +1,9 @@
-import math
-r=int(input("Enter the radius of the circle :"))
-a=math.pi*r*r
-p=2*math.pi*r
-print("The area of the circle =",a)
-print("The perimeter of the circle =",p)
+# import math
+r=int(input("enter radius"))
+pi=3.14
+area=pi*r*r
+perimeter=2*pi*r
+print(f"area of circle is {area}")
+print(f"perimeter of circle is {perimeter}")
+
+# area and perimeter of circle
