5a23ec78d88f723b5e1137ab21d99d347d36cafc
Jayalekshmi M <ammusz749@gmail.com>
2025-01-06 21:18:26 +0530

    Add files via upload

---
+++ digits.py
@@ -0,0 +1,19 @@
+num=input("enter a number")
+print(f"number of digits in {num}={len(num)}")
+
+# number of digits in a number
+
+
+
+
+
+# num = input("Enter a number: ")
+
+# # Check if the input is a valid number
+# if num.lstrip('-').isdigit():  # Allow an optional '-' at the start
+#     num_digits = len(num.lstrip('-'))  # Exclude the '-' sign, if present
+#     print(f"Number of digits in {num} = {num_digits}")
+# else:
+#     print("Invalid input! Please enter a valid number.")
+
+# improved code to neglect the - sign
