1024631fbdb8599063a7b23eee926ca85a107e0a
Jayalekshmi M <ammusz749@gmail.com>
2025-01-06 21:30:19 +0530

    completed gcd

--- gcd.py
+++ gcd.py
@@ -1,9 +1,22 @@
 def gcd(a,b):
-    if(b==0):
-        print("GCD = ",a)
+    if b==0:
+        return a
     else:
         return gcd(b,a%b)
-e=int(input("Enter a :"))
-d=int(input("Enter b :"))
-gcd(e,d)
+num1=int(input("enter first number"))
+num2=int(input("enter second number"))
+result=gcd(num1,num2)
+print(f"gcd of {num1} and {num2} is {result}")
+
+# for calculating gcd of 2 numbers usin function
+
+
+
+import math
+num1=int(input("enter first number"))
+num2=int(input("enter second number"))
+result=math.gcd(num1,num2)
+print(f"gcd is {result}")
+
+#simpler method using built in function
 
